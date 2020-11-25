b8ca19692b9e0e2f076323b06607757e03162dbb
cochimu <cochimu1221@icloud.com>
2020-11-25 21:48:26 +0900

    バリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    validates :nickname
+    validates :birth_day
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birth_day, presence: true
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+      validates :last_name
+      validates :first_name
+    end
 
-  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  validates :password_confirmation, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+
+    with_options format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i } do
+      validates :password
+      validates :password_confirmation
+    end
+  end
 
   has_many :items
   has_many :comments
