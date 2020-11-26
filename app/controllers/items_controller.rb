b8672020c79c690e42a4f7d82b33a0d39c08eb64
cochimu <cochimu1221@icloud.com>
2020-11-26 17:49:05 +0900

    ActiveStorage実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @items = Item.all
   end
@@ -11,17 +11,13 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.create(item_params)
-    if @item.save
       redirect_to root_path
-    else
-      render :new
-    end
   end
 
   private
   
   def item_params
-    params.require(:item).permit(:name, :price, :category_id, :charge_id, :day_id, :prefecture_id, :status_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :price, :introduction, :image, :category_id, :charge_id, :day_id, :prefecture_id, :status_id).merge(user_id: current_user.id)
   end
 
 end
