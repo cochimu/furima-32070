61d28974c5210183f4be7ab0a4c17395d0095e00
cochimu <cochimu1221@icloud.com>
2020-11-26 23:52:14 +0900

    FactoryBot記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,17 @@
 FactoryBot.define do
   factory :item do
+    name         { Faker::Game.title }
+    introduction { Faker::Game.genre }
+    price        { Faker::Number.between(from: 300, to: 9999999) }
+    association :category
+    association :status
+    association :charge
+    association :prefecture
+    association :day
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'create' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+
+    context '商品の出品ができる時' do
+      it '全ての項目が存在すれば登録できること' do
+      end
+    end
+
+    context '商品の出品ができない時' do
+      it '商品画像がないと登録できないこと' do
+      end
+
+      it '商品名がないと登録できないこと' do
+      end
+
+      it '商品の説明がないと登録できないこと' do
+      end
+
+      it 'カテゴリーの情報がないと登録できないこと' do
+      end
+
+      it '商品の状態についての情報がないと登録できないこと' do
+      end
+
+      it '配送料の負担についての情報がないと登録できないこと' do
+      end
+
+      it '発送元の地域の情報がないと登録できないこと' do
+      end
+
+      it '発送までの日数の情報がないと登録できないこと' do
+      end
+
+      it '価格の入力がないと登録できないこと' do
+      end
+
+      it '価格の範囲が¥300~¥9999999でないと登録できないこと' do
+      end
+
+      it '価格は半角数字でないと登録できないこと' do
+      end
+    end
+  end
 end
