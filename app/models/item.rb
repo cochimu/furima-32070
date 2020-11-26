440dc9f971b29f408d53e3a074b8c2e6fa20867a
cochimu <cochimu1221@icloud.com>
2020-11-26 14:35:45 +0900

    アソシエーション、バリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,24 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :charge
+  belongs_to :day
+  belongs_to :prefecture
+  belongs_to :status
+
+  with_options presence: true do
+    validates :name
+    validates :price
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :charge_id
+    validates :day_id
+    validates :prefecture_id
+    validates :status_id
+  end
 end
