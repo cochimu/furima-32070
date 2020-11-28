adf070ece56e09d648c26791dd7ac9841fec3a12
cochimu <cochimu1221@icloud.com>
2020-11-28 18:08:24 +0900

    挙動確認実施（Rubocop済）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:edit]
+  before_action :barrier_user, only: [:edit]
+
   def index
     @items = Item.all.order(created_at: :DESC)
   end
@@ -28,9 +31,22 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :price, :introduction, :image, :category_id, :charge_id, :day_id, :prefecture_id, :status_id).merge(user_id: current_user.id)
+  end
+
+  def barrier_user
+    redirect_to root_path unless Item.find(params[:id]).user.id == current_user.id
   end
 end
