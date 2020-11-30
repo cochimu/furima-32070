f681e1bced69e2a5e7bb22a3e33acc982d5a8fd3
cochimu <cochimu1221@icloud.com>
2020-11-30 11:24:29 +0900

    エラーメッセージ実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,27 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @user_order = UserOrder.new
   end
 
   def create
-    #binding.pry
+    @user_order = UserOrder.new(order_params)
+    if @user_order.valid?
+      @user_order.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
+  private
+
+    def order_params
+      params.require(:user_order).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :item_id)
+    end
+
+    def set_item
+      @item = Item.find(params[:item_id])
+    end
 end
--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,13 +1,13 @@
 class UserOrder
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture, :city, :address, :building_name, :phone_number
+  attr_accessor :post_code, :prefecture_id, :city, :address, :building_name, :phone_number
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :address
   end
-  validates :prefecture, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :phone_number, format: {with: /\A\d{11}\z/ }
 
   def save
