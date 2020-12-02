b958c0aa3f39768987dfe98ab4698cd9a0a1d5f9
cochimu <cochimu1221@icloud.com>
2020-12-02 17:21:00 +0900

    修正

--- spec/models/user_order_spec.rb
+++ spec/models/user_order_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe UserOrder, type: :model do
   describe '購入情報の保存' do
     before do
-      @user_order = FactoryBot.build(:user_order)
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @user_order = FactoryBot.build(:user_order, user_id: user.id, item_id: item.id)
     end
 
     context '商品の購入ができる時' do
@@ -14,61 +16,61 @@ RSpec.describe UserOrder, type: :model do
 
     context '商品の購入ができない時' do
       it 'tokenが空では登録できないこと' do
-        @user_order.token = nil
+        @user_order.token = ''
         @user_order.valid?
         expect(@user_order.errors.full_messages).to include("Token can't be blank")
       end
 
       it 'post_codeが空だと購入できないこと' do
-        @user_order.post_code = nil
+        @user_order.post_code = ''
         @user_order.valid?
         expect(@user_order.errors.full_messages).to include("Post code can't be blank")
       end
 
       it 'post_codeにハイフンがないと登録できないこと' do
-        @user_order.post_code = 1_234_567
+        @user_order.post_code = '1234567'
         @user_order.valid?
         expect(@user_order.errors.full_messages).to include('Post code is invalid. Include hyphen(-)')
       end
 
       it 'prefecture_idが空だと登録できないこと' do
-        @user_order.prefecture_id = nil
+        @user_order.prefecture_id = ''
         @user_order.valid?
         expect(@user_order.errors.full_messages).to include("Prefecture can't be blank")
       end
 
-      it 'prefectureのが1だと登録できないこと' do
+      it 'prefecture_idが1だと登録できないこと' do
         @user_order.prefecture_id = 1
         @user_order.valid?
         expect(@user_order.errors.full_messages).to include("Prefecture can't be blank")
       end
 
       it 'cityが空だと登録できないこと' do
-        @user_order.city = nil
+        @user_order.city = ''
         @user_order.valid?
         expect(@user_order.errors.full_messages).to include("City can't be blank")
       end
 
       it 'addressが空だと登録できないこと' do
-        @user_order.address = nil
+        @user_order.address = ''
         @user_order.valid?
         expect(@user_order.errors.full_messages).to include("Address can't be blank")
       end
 
       it 'phone_numberが空だと登録できないこと' do
-        @user_order.phone_number = nil
+        @user_order.phone_number = ''
         @user_order.valid?
         expect(@user_order.errors.full_messages).to include('Phone number is invalid')
       end
 
       it 'phone_numberにハイフンがあると登録できないこと' do
-        @user_order.phone_number = 123 - 1234 - 5678
+        @user_order.phone_number = '123-1234-5678'
         @user_order.valid?
         expect(@user_order.errors.full_messages).to include('Phone number is invalid')
       end
 
       it 'phone_numberは11桁以内であること' do
-        @user_order.phone_number = 123 - 1234 - 56_789
+        @user_order.phone_number = '123123456789'
         @user_order.valid?
         expect(@user_order.errors.full_messages).to include('Phone number is invalid')
       end
